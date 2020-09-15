87ce94964027967c4162d0cec26b1b500584d518
NoaFilosof <noafilosof@hotmail.co.uk>
2020-09-15 17:13:49 +0100

    artshow page built with tiles

--- frontend/src/components/Artist/ArtShow.js
+++ frontend/src/components/Artist/ArtShow.js
@@ -66,39 +66,55 @@ class ArtShow extends React.Component {
       return (
         <section className="hero is-fullheight-with-navbar">
           <div className="hero-body">
-            <div className="container artShowContainer">
+            <div className="container artShowContainer tile is-ancestor is-vertical">
 
-              <div className="column auto">
-              </div>
-
-              <div className="column is-two-fifths artShowMain">
-                <div className="artName artContent">
-                  <h1 className="title">{this.state.art.name}</h1>
-                </div>
-
-                <div className="artContent">
-                  <img className="image imgArtShow" src={this.state.art.image} alt={this.state.art.descripton}/>
-                </div>
-                <div className="artContent">
-                  <h2>By {this.state.art.owner.username}</h2>
-                </div>
-                <div className="artContent columns">
-                  <div className="column">
-                    <p>A bit about this piece:</p>
-                    <p>{this.state.art.description}</p>
+              {/* <div className="column auto">
+              </div> */}
+              <div className="tile is-parent">
+                <div className="column is-3 artShowMain tile is-parent is-vertical">
+                  <div className="artName artContent tile is-child">
+                    <h1 className="title">{this.state.art.name}</h1>
                   </div>
-                  <div className="column">
-                    <p>£{this.state.art.price}</p>
+                  <div className="artContent tile is-child">
+                    <h2>By {this.state.art.owner.username}</h2>
+                  </div>
+                  <div className="artContent">
+                    <div className="column tile is-child">
+                      <p>A bit about this piece:</p>
+                      <p>{this.state.art.description}</p>
+                    </div>
                   </div>
                 </div>
+
+                <div className="tile is-child is-7">
+                  <img className="image has-ratio imgArtShow" src={this.state.art.image} alt={this.state.art.descripton}/>
+                </div>
+
+                <div className="tile is-child is-1">
+                  <div className="column is-one-fifth artShowEnquire tile is-parent  is-vertical">
+
+                    { (!isOwner && isLoggedIn) ?
+                      <button className="button enquireBtn" onClick={this.handleEnquire}>Enquire</button>
+                      :
+                      <p></p>
+                    }
+                  </div>
+
+                  <div className="artContent columns">
+                    <div className="column price">
+                      <p>£{this.state.art.price}</p>
+                    </div>
+                  </div>
+                </div>
               </div>
 
-              <div className="column auto">
-              </div>
-
-              <div className="column is-one-fifth artShowEnquire">
+              <div className="tile is-child">
                 { isOwner ?
                   <div>
+                    <div className="tile icons">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleDelete} className="deleteIcon"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEdit} className="editIcon"><path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/></svg>
+                    </div>
                     <p>Enquiries:</p>
                     {this.state.art.enquiries.map((enq)  =>
                       <div key={enq.id}>
@@ -107,22 +123,11 @@ class ArtShow extends React.Component {
                       </div>
                     )
                     }
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleDelete} className="deleteIcon"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEdit} className="editIcon"><path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/></svg>
                   </div>
                   :
                   <div></div>
                 }
-                { (!isOwner && isLoggedIn) ?
-                  <button className="button enquireBtn" onClick={this.handleEnquire}>Enquire</button>
-                  :
-                  <p></p>
-                }
               </div>
-
-              <div className="column auto">
-              </div>
-
             </div>
           </div>
         </section>
