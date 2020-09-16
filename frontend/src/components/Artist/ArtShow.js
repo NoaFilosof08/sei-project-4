b3db2606f939cd9329e1283088c4439036ddbcd3
Eliza Thompson <Eliza_thompson@icloud.com>
2020-09-16 15:39:25 +0100

    art show page updates

--- frontend/src/components/Artist/ArtShow.js
+++ frontend/src/components/Artist/ArtShow.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { getSingleArt, getSingleUnpopulatedProfile, deleteArt } from '../../lib/api'
 import { isAuthenticated } from '../../lib/auth'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class ArtShow extends React.Component {
 
@@ -86,11 +86,11 @@ class ArtShow extends React.Component {
                   </div>
                 </div>
 
-                <div className="tile is-child is-7">
+                <div className="tile is-child is-7 artImgShow">
                   <img className="image has-ratio imgArtShow" src={this.state.art.image} alt={this.state.art.descripton}/>
                 </div>
 
-                <div className="tile is-child is-2">
+                <div className="tile is-child is-2 artContent">
                   <div className="artShowEnquire tile is-parent is-vertical">
 
                     { (!isOwner && isLoggedIn) ?
@@ -99,11 +99,8 @@ class ArtShow extends React.Component {
                       <p></p>
                     }
                   </div>
-
-                  <div className="artContent columns">
-                    <div className="column price">
-                      <p><strong>Price:</strong> £{this.state.art.price}</p>
-                    </div>
+                  <div className="price">
+                    <p><strong>Price:</strong> £{this.state.art.price}</p>
                   </div>
                 </div>
               </div>
@@ -111,17 +108,28 @@ class ArtShow extends React.Component {
               <div className="tile is-child">
                 { isOwner ?
                   <div>
+                    <p className="controls">Controls:</p>
                     <div className="tile icons">
+                      <br></br>
+                      <br></br>
+                      <br></br>
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleDelete} className="deleteIcon"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEdit} className="editIcon"><path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/></svg>
                     </div>
+                    <hr></hr>
                     <div className="enquiries">
                       <p>Enquiries:</p>
                       {this.state.art.enquiries.map((enq)  =>
-                        <div key={enq.id} className="enquiry">
-                          <p><strong>{enq.owner.username}</strong>: </p>
-                          <p>{enq.text}</p>
-                        </div>
+                        <>
+                          <div key={enq.id} className="enquiry">
+
+                            <p><strong>Enquiry from <Link to={`/artist/${enq.owner.id}`}><span>{enq.owner.username}</span></Link></strong>: </p>
+
+                            <p>{enq.text}</p>
+
+                          </div>
+                          <br></br>
+                        </>
                       )
                       }
                     </div>
@@ -129,6 +137,7 @@ class ArtShow extends React.Component {
                   :
                   <div></div>
                 }
+                <hr></hr>
               </div>
             </div>
           </div>
