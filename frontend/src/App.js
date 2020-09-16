9120b9518b4d789780c72677aebc9221d9b3eb90
Eliza Thompson <Eliza_thompson@icloud.com>
2020-09-16 17:50:45 +0100

    small clean up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Notifications from 'react-notify-toast'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import NavBar from './components/Common/NavBar'
 import Home from './components/Common/Home'
@@ -24,7 +23,6 @@ const App = () => (
     <main>
       <NavBar />
       <Notifications />
-      <FontAwesomeIcon icon={['fab', 'apple']} />
       <Switch>
         <Route exact path="/" component={Splash} />
         <Route path="/art/edit/:id" component={EditArt} />
--- frontend/src/components/Artist/ArtShow.js
+++ frontend/src/components/Artist/ArtShow.js
@@ -111,12 +111,11 @@ class ArtShow extends React.Component {
                   <div>
                     <p className="controls typeBubble subtitle">Controls:</p>
                     <div className="tile icons">
+                      {/* <br></br>
                       <br></br>
-                      <br></br>
-                      <br></br>
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleDelete} className="deleteIcon"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
-
-                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEdit} className="">
+                      <br></br> */}
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleDelete} className="editIcon"><path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z"/></svg>
+                      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEdit} className="deleteIcon">
                         <path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/>
                       </svg>
                     </div>
