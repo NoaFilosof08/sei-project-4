beca24a2396616ff02df401347a035283c48f921
NoaFilosof <noafilosof@hotmail.co.uk>
2020-09-16 17:31:03 +0100

    icons sorted

--- frontend/src/components/Artist/ProfilePage.js
+++ frontend/src/components/Artist/ProfilePage.js
@@ -1,5 +1,5 @@
 // import { fab } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { getSingleProfile } from '../../lib/api'
 import { Link } from 'react-router-dom'
@@ -48,6 +48,7 @@ class ProfilePage extends React.Component {
   }
 
   render() {
+    console.log(this.state.user)
     if (!this.state.user) {
       console.log('no user logged in')
       return null
@@ -91,15 +92,14 @@ class ProfilePage extends React.Component {
                   </>
                 }
 
-
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEditProfile} className="editIcon"><path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/>
-                </svg>
                 <div>
-                  <i className="fab fa-instagram">
-                    {/* <FontAwesomeIcon icon={['fab', 'apple']} /> */}
-                  </i>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={this.handleEditProfile} className="editIcon"><path d="M18.363 8.464l1.433 1.431-12.67 12.669-7.125 1.436 1.439-7.127 12.665-12.668 1.431 1.431-12.255 12.224-.726 3.584 3.584-.723 12.224-12.257zm-.056-8.464l-2.815 2.817 5.691 5.692 2.817-2.821-5.693-5.688zm-12.318 18.718l11.313-11.316-.705-.707-11.313 11.314.705.709z"/>
+                  </svg>
+                  <a href={this.state.user.insta_handle} target="_blank" without rel="noreferrer">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" className="editIcon"><path d="M14.52,2.469H5.482c-1.664,0-3.013,1.349-3.013,3.013v9.038c0,1.662,1.349,3.012,3.013,3.012h9.038c1.662,0,3.012-1.35,3.012-3.012V5.482C17.531,3.818,16.182,2.469,14.52,2.469 M13.012,4.729h2.26v2.259h-2.26V4.729z M10,6.988c1.664,0,3.012,1.349,3.012,3.012c0,1.664-1.348,3.013-3.012,3.013c-1.664,0-3.012-1.349-3.012-3.013C6.988,8.336,8.336,6.988,10,6.988 M16.025,14.52c0,0.831-0.676,1.506-1.506,1.506H5.482c-0.831,0-1.507-0.675-1.507-1.506V9.247h1.583C5.516,9.494,5.482,9.743,5.482,10c0,2.497,2.023,4.52,4.518,4.52c2.494,0,4.52-2.022,4.52-4.52c0-0.257-0.035-0.506-0.076-0.753h1.582V14.52z"></path>
+                    </svg>
+                  </a>
                 </div>
-
 
                 <br></br>
               </div>
